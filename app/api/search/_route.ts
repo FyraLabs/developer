6e196de784a8da12199e19760f226879a950f5d5
Lleyton Gray <lleyton@fyralabs.com>
2024-08-26 00:31:43 -0700

    disable search

--- app/api/search/_route.ts
+++ app/api/search/_route.ts
@@ -1,8 +1,6 @@
 import { getPages } from "@/app/source";
 import { createSearchAPI } from "fumadocs-core/search/server";
 
-export const runtime = "edge";
-
 export const { GET } = createSearchAPI("advanced", {
   indexes: getPages().map((page) => ({
     title: page.data.title,
